05f8dce9551bfa1b693243795f5a27de42a4ae8f
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-19 20:40:22 +0100

    added index.css

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -1,18 +1,30 @@
 //import { useState } from 'react'
 import './App.css'
 
-function App() {
+const messages = [
+  "Learn React",
+  "Apply for job",
+  "Money"
+];
 
+
+function App() {
+  const step = 2
 
   return (
     <>
       <div className='steps'>
         <div className='numbers'>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className={`${step >=1 ? "active" : ""}`}>1</div>
+          <div className={`${step >=1 ? "active" : ""}`}>2</div>
+          <div className={`${step >=1 ? "active" : ""}`}>3</div>
         </div>
       </div>
+
+      <p className='message'>Step {step}: {messages[step<1]}</p>
+      <div className='buttons'></div>
+        <button style={{backgroundColor: "#ff3645", color: "#fff"}}>Previous</button>
+        <button style={{backgroundColor: "#ff9954", color: "#fff"}}>Next</button>
 
     </>
   )
