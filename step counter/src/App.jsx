8aa32c76587ed84a6eed42d2a3f1fe37a0e1828e
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-19 20:42:11 +0100

    added index.css

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -9,19 +9,20 @@ const messages = [
 
 
 function App() {
-  const step = 2
+  const step = 1
 
   return (
     <>
       <div className='steps'>
         <div className='numbers'>
           <div className={`${step >=1 ? "active" : ""}`}>1</div>
-          <div className={`${step >=1 ? "active" : ""}`}>2</div>
-          <div className={`${step >=1 ? "active" : ""}`}>3</div>
+          <div className={`${step >=2 ? "active" : ""}`}>2</div>
+          <div className={`${step >=3 ? "active" : ""}`}>3</div>
         </div>
       </div>
 
-      <p className='message'>Step {step}: {messages[step<1]}</p>
+      <p className='message'>Step {step}: {messages[step -1]}</p>
+
       <div className='buttons'></div>
         <button style={{backgroundColor: "#ff3645", color: "#fff"}}>Previous</button>
         <button style={{backgroundColor: "#ff9954", color: "#fff"}}>Next</button>
