50ef8caf08afaca07f35d0c264acfe4193b40954
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-20 21:05:18 +0100

    created handle function and fix a bug were the step would go more that 3

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -1,4 +1,5 @@
 //import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 
 const messages = [
@@ -9,13 +10,15 @@ const messages = [
 
 
 function App() {
-  const step = 1
+  const [step, setStep] = useState(1)
 
   function handlePrevious(){
-
+    if (step >1)
+     setStep(step -1)
   }
   function handleNext(){
-
+    if (step < 3)
+    setStep(step + 1)
   }
 
 
@@ -23,16 +26,15 @@ function App() {
     <>
       <div className='steps'>
         <div className='numbers'>
-          <div className={`${step >=1 ? "active" : ""}`}>1</div>
-          <div className={`${step >=2 ? "active" : ""}`}>2</div>
-          <div className={`${step >=3 ? "active" : ""}`}>3</div>
+          <div className={step >=1 ? "active" : ""}>1</div>
+          <div className={step >=2 ? "active" : ""}>2</div>
+          <div className={step >=3 ? "active" : ""}>3</div>
         </div>
       </div>
 
       <p className='message'>Step {step}: {messages[step -1]}</p>
 
       <div className='buttons'></div>
-      //We use onClick and call an arrow function that does something specific
         <button style={{backgroundColor: "#ff3645", color: "#fff"}} onClick={(handlePrevious)}>Previous</button>
         <button style={{backgroundColor: "#ff9954", color: "#fff"}} onClick={(handleNext)}>Next</button>
 
