8df9da14a6b15b800c8fc956d0836945fb1114c1
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-21 21:39:04 +0100

    created logic for opening and closing window

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -11,6 +11,7 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1)
+  const [isOpen,setIsOpen] = useState(true)
 
   function handlePrevious(){
     if (step >1)
@@ -23,22 +24,25 @@ function App() {
 
 
   return (
-    <>
-      <div className='steps'>
-        <div className='numbers'>
-          <div className={step >=1 ? "active" : ""}>1</div>
-          <div className={step >=2 ? "active" : ""}>2</div>
-          <div className={step >=3 ? "active" : ""}>3</div>
+    <div>
+      <button className='close' onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+        <div className='steps'>
+          <div className='numbers'>
+            <div className={step >=1 ? "active" : ""}>1</div>
+            <div className={step >=2 ? "active" : ""}>2</div>
+            <div className={step >=3 ? "active" : ""}>3</div>
+          </div>
+          <p className='message'>
+            Step {step}: {messages[step -1]}
+          </p>
+        <div className='buttons'>
+          <button style={{backgroundColor: "#ff3645", color: "#fff"}} onClick={(handlePrevious)}>Previous</button>
+          <button style={{backgroundColor: "#ff9954", color: "#fff"}} onClick={(handleNext)}>Next</button>
         </div>
       </div>
-
-      <p className='message'>Step {step}: {messages[step -1]}</p>
-
-      <div className='buttons'></div>
-        <button style={{backgroundColor: "#ff3645", color: "#fff"}} onClick={(handlePrevious)}>Previous</button>
-        <button style={{backgroundColor: "#ff9954", color: "#fff"}} onClick={(handleNext)}>Next</button>
-
-    </>
+      )}
+    </div>
   )
 }
 
