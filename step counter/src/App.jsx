70f7dd8b333103e69ffaf550caef7001ba4a3d23
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-22 19:26:44 +0100

    making a challenge

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -2,9 +2,23 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  return(
+  const [step,setStep] =  useState(1)
+  const [count,setCounter] = useState(1)
 
+  return(
+    <>
+      <div className='container'>
+        <Counter/>
+      </div>
+    </>
   )
+}<div className='buttons'>
+        <button style={{backgroundColor: "#ff3645", color: "#fff"}} onClick={(handlePrevious)}>Previous</button>
+        <button style={{backgroundColor: "#ff9954", color: "#fff"}} onClick={(handleNext)}>Next</button>
+      </div>
+
+ function Counter(){
+
 }
 
 export default App
