59e32d7b9b7239904f88b3e516309da6f13c9f4b
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-22 21:09:14 +0100

    endend challenge for creating counter with date

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [step,setStep] =  useState(1)
-  const [count,setCounter] = useState(1)
+
 
   return(
     <>
@@ -12,13 +11,42 @@ function App() {
       </div>
     </>
   )
-}<div className='buttons'>
-        <button style={{backgroundColor: "#ff3645", color: "#fff"}} onClick={(handlePrevious)}>Previous</button>
-        <button style={{backgroundColor: "#ff9954", color: "#fff"}} onClick={(handleNext)}>Next</button>
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
 
- function Counter(){
+      <div>
+        <button onClick={() => setCount((c) => c - step)} style={{backgroundColor: "#ff3645", color: "#fff"}}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)} style={{backgroundColor: "#3cb371", color: "#fff"}}>+</button>
+      </div>
 
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
 }
+
 
 export default App
