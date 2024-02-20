8573eee10a893ab5356c42ab38d770542d94aa7f
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-20 20:10:54 +0100

    Children for Message props

--- step counter/src/App.jsx
+++ step counter/src/App.jsx
@@ -33,9 +33,8 @@ function App() {
             <div className={step >=2 ? "active" : ""}>2</div>
             <div className={step >=3 ? "active" : ""}>3</div>
           </div>
-          <p className='message'>
-            Step {step}: {messages[step -1]}
-          </p>
+
+         <StepMessage step={step}>{messages[step-1]}</StepMessage>
         <div className='buttons'>
           <Button
             bgColor="#7905f2"
@@ -50,6 +49,14 @@ function App() {
       </div>
       )}
     </div>
+  )
+}
+function StepMessage(step,children){
+  return(
+  <div className='message'>
+    <h3>Step {step}</h3>
+      {children}
+  </div>
   )
 }
 
